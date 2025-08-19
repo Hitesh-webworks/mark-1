aeba5f35d79413a8d13f9e550af65ec9c9dd9812
Hitesh-webworks <hitesh.webworks@gmail.com>
2025-08-19 20:21:44 +0000

    Fix vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,12 @@
-export default {
-  base: '/',
-}
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    host: true,   // required for Codespaces
+    port: 5173,   // or whatever Codespaces gives you
+  },
+  base: '/', // ✅ for custom domain (vidyalayweb.space)
+})
